c23da0a3e3f7fab1b644a17e25ec972966d3162c
OlegSkal <SOV.develop.ua@gmail.com>
2026-09-20 23:21:47 +0300

    chore: re-cache git index, clean obsolete scripts and add model archive

--- scripts/_FIX_UNIQUE_error_.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "%~dp0"
-chcp 65001 > nul
-
-python build_offline_db_fix_UNIQUE_error.py %*
-pause
--- scripts/_START_.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd /d "%~dp0"
-chcp 1251 > nul
-
-"python.exe" build_offline_db.py
